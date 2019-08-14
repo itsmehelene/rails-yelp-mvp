2f57c1514c92c6d0a2673d9473028c70e03dda45
helene frstemjak <frsteme@hotmail.com>
2019-08-14 15:50:08 +0200

    full yelp

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:new, :create]
+  end
+
+  # A visitor can see the list of all restaurants.
+  # get 'restaurants', to: 'restaurants#index'
+
+  # # She/He can add a new restaurant, and be redirected to the show view of that new restaurant.
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post 'restaurants', to: 'restaurants#create'
+
+  # # She/He can see the details of a restaurant, with all the reviews related to the restaurant.
+  # get 'restaurants/38', to: ''
+  # get 'restaurants/38/reviews/new', to: ''
+
+  # # She/He can add a new review to a restaurant
+  # post 'restaurants/38/reviews', to: ''
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '345678',
+    category:        "italian",
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '34567890',
+    category:        "chinese",
+  },
+  {
+    name:         'Gummy Bears',
+    address:      '7 Boundary St, Milan E2 7JE',
+    phone_number:  '34567890°',
+    category:        "japanese",
+  },
+  {
+    name:         'Salted Caramel',
+    address:      '8 Boundary St, London E2 7JE',
+    phone_number:  '567890',
+    category:        "belgian",
+  },
+  {
+    name:         'Cookies and co',
+    address:      '7 Boundary Road, London E2 7JE',
+    phone_number:  '0987654',
+    category:        "french",
+  },
+  {
+    name:         'Cake pop',
+    address:      '6 Queen St, London E2 7JE',
+    phone_number:  '98456900',
+    category:        "french",
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
